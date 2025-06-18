68d2ec50f72cc4a6da8ecd7f851f0d63c39de986
lannd <duclan0710@gmail.com>
2025-06-18 13:07:47 +0700

    angular tutorial

---
+++ product-list-app/src/app/shared/header-layout/header-layout.module.ts
@@ -0,0 +1,9 @@
+// import { NgModule } from '@angular/core';
+
+// import { HeaderLayoutComponent } from './header-layout.component';
+// import { CommonModule } from '@angular/common';
+
+// @NgModule({
+//   imports: [CommonModule, HeaderLayoutComponent],
+// })
+// export class HeaderLayoutModule {}
